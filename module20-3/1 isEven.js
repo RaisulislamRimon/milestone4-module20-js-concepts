8e33f756de05c9f8c0b8df8a079b4a163efb0237
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-04 19:17:21 +0000

    upfspce

---
+++ module20-3/1 isEven.js
@@ -0,0 +1,8 @@
+// 20-3 Check even and odd number using function
+
+function isEven(value) {
+  return value % 2 === 0;
+}
+
+var checkEven = isEven(30);
+console.log(checkEven, typeof checkEven, );
