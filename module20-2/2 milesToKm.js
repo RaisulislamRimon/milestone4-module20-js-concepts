db9add08d7bb86a422b31887eedfaa9dafdd0b15
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-04 19:09:00 +0000

    milesToKm

---
+++ module20-2/2 milesToKm.js
@@ -0,0 +1,15 @@
+/* Convert to km  from miles */
+
+function milesToKm(miles) {
+  const km = miles * 1.60934;
+  //   return km;
+  console.log(km);
+}
+// milesToKm(1000);
+// console.log(milesToKm(1000));
+
+// milesToKm(2);
+milesToKm(2);
+
+// meter to cm
+// gram to kg
