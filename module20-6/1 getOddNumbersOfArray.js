3b8e308c15272a8046cebe882bc8a0b9a96effca
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-05 09:11:35 +0000

    return sumOfAllOddNumbers

--- module20-6/1 getOddNumbersOfArray.js
+++ module20-6/1 getOddNumbersOfArray.js
@@ -14,7 +14,7 @@ function getOddNumbersOfArray(oddNumbers) {
 var nums = [5, 7, 8, 10, 45, 30];
 getOddNumbersOfArray(nums);
  */
-
+/* 
 function getOddNumbersOfArray(oddNumbers) {
   var total = 0;
   for (var i = 0; i < oddNumbers.length; i++) {
@@ -28,3 +28,20 @@ function getOddNumbersOfArray(oddNumbers) {
 }
 var nums = [5, 7, 8, 10, 45, 30];
 getOddNumbersOfArray(nums);
+ */
+
+function getOddNumbersOfArray(oddNumbers) {
+  var total = 0;
+  for (var i = 0; i < oddNumbers.length; i++) {
+    if (oddNumbers[i] % 2 !== 0) {
+      console.log("odd numbers : ", oddNumbers[i]);
+      var element = oddNumbers[i];
+      total += element;
+    }
+  }
+  return total;
+}
+var nums = [5, 7, 8, 10, 45, 30];
+var sumOfAllOddNumbers = getOddNumbersOfArray(nums);
+
+console.log("Total of all odd numbers in the array : ", sumOfAllOddNumbers);
