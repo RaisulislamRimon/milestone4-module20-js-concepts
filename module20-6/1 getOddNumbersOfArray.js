3ba74a5427cb0e4f1c2de3836404eaef81948f52
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-05 11:32:14 +0000

    update

--- module20-6/1 getOddNumbersOfArray.js
+++ module20-6/1 getOddNumbersOfArray.js
@@ -30,6 +30,7 @@ var nums = [5, 7, 8, 10, 45, 30];
 getOddNumbersOfArray(nums);
  */
 
+/* 
 function getOddNumbersOfArray(oddNumbers) {
   var total = 0;
   for (var i = 0; i < oddNumbers.length; i++) {
@@ -45,3 +46,40 @@ var nums = [5, 7, 8, 10, 45, 30];
 var sumOfAllOddNumbers = getOddNumbersOfArray(nums);
 
 console.log("Total of all odd numbers in the array : ", sumOfAllOddNumbers);
+ */
+
+/* 
+function getOddNumbersOfArray(numbers) {
+  const oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const index = i;
+    const element = numbers[index];
+    if (element % 2 !== 0) {
+      console.log(index, element);
+      oddNumbers.push(element);
+    }
+  }
+  return oddNumbers;
+  // console.log(oddNumbers);
+}
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+var resultOfOddNumbers = getOddNumbersOfArray(myNumbers);
+console.log(resultOfOddNumbers);
+ */
+
+function getOddNumbersOfArray(numbers) {
+  const oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const index = i;
+    const element = numbers[index];
+    if (element % 2 !== 0) {
+      console.log(index, element);
+      oddNumbers.push(element);
+    }
+  }
+  return oddNumbers;
+  // console.log(oddNumbers);
+}
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+var resultOfOddNumbers = getOddNumbersOfArray(myNumbers);
+console.log(resultOfOddNumbers);
