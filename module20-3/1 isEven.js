ab272575c06c5d95c7930f94892cdc32d6f4ae60
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-05 04:41:04 +0000

    even number checker

--- module20-3/1 isEven.js
+++ module20-3/1 isEven.js
@@ -1,8 +1,58 @@
 // 20-3 Check even and odd number using function
 
-function isEven(value) {
+/* function isEven(number) {
+  const remainder = number % 2;
+  console.log(remainder);
+}
+
+isEven(30);
+isEven(31); */
+
+/* function isEven(value) {
+  console.log(value);
   return value % 2 === 0;
+} 
+
+var checkEven = isEven(30);
+console.log(checkEven, typeof checkEven);
+var checkEven = isEven(23);
+console.log(checkEven, typeof checkEven);
+var checkEven = isEven(24);
+console.log(checkEven, typeof checkEven);
+*/
+/* 
+function isEven(num) {
+  var remainder = num % 2;
+  console.log(remainder);
+  if (remainder === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 var checkEven = isEven(30);
-console.log(checkEven, typeof checkEven, );
+// var checkEven = isEven(31);
+
+// if (checkEven===true) {
+if (checkEven) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+ */
+
+function isEven(num) {
+  var remainder = num % 2;
+  console.log(remainder);
+  if (remainder === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var checkEven = isEven(30);
+console.log(checkEven);
+var checkEven = isEven(31);
+console.log(checkEven);
