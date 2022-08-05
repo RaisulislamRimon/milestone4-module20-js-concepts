5ca2bc0fecb7b0d073830510b514ba3d360cfd52
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-05 08:48:06 +0000

    findOddSum.js

--- module20-5/1 findOddSum.js
+++ module20-5/1 findOddSum.js
@@ -14,15 +14,47 @@
 
 // var numbers = [5, 7, 8, 10, 45, 30];
 // getSumOfAnArray(numbers);
-
+/* 
 function getSumOfAnArray(numbers) {
   var total = 0;
   for (var i = 0; i < numbers.length; i++) {
     // total = total + numbers[i];
     total += numbers[i];
   }
-  console.log(total);
+  // console.log(total);
+  return total;
 }
 
 var numbers = [5, 7, 8, 10, 45, 30];
-getSumOfAnArray(numbers);
+var result= getSumOfAnArray(numbers);
+console.log(result); */
+
+/* 
+var numbers = [5, 7, 8, 10, 45, 30];
+var result = numbers.reduce((total, number) => {
+  return total + number;
+}, 0);
+console.log(result);
+ */
+/* 
+function myNumbers(num) {
+  console.log(num);
+}
+
+var numbers = [5, 7, 8, 10, 45, 30];
+
+myNumbers(10);
+ */
+
+function myNumbers(num) {
+  var total = 0;
+  for (var i = 0; i < num.length; i++) {
+    total = total + num[i];
+  }
+  return total;
+}
+
+var numbers = [5, 7, 8, 10, 45, 30];
+
+var sum = myNumbers(numbers);
+console.log(sum);
