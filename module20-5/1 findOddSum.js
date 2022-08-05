0190bd2b5e82798315d4310abf00e49988f10da2
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-05 05:44:51 +0000

    function getSumOfAnArray(numbers)

--- module20-5/1 findOddSum.js
+++ module20-5/1 findOddSum.js
@@ -1,15 +1,27 @@
 /* 20-5 Calculate Sum of all numbers of an array */
 // Practice Problem 3
 
+// function getSumOfAnArray(numbers) {
+//   //   console.log("getSumOfAnArray: " + numbers);
+//   console.log(numbers);
+//   /* for (var i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+//   } */
+//   numbers.forEach((number) => {
+//     console.log(number);
+//   });
+// }
+
+// var numbers = [5, 7, 8, 10, 45, 30];
+// getSumOfAnArray(numbers);
+
 function getSumOfAnArray(numbers) {
-  //   console.log("getSumOfAnArray: " + numbers);
-  console.log(numbers);
-  /* for (var i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-  } */
-  numbers.forEach((number) => {
-    console.log(number);
-  });
+  var total = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    // total = total + numbers[i];
+    total += numbers[i];
+  }
+  console.log(total);
 }
 
 var numbers = [5, 7, 8, 10, 45, 30];
